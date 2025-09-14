a76b73260b5283093af6c6211fac36dc34d25313
TothAndrasBalint <andrastothbalint@gmail.com>
2025-09-14 12:39:39 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,8 +1,6 @@
-function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('pong');
+export function GET() {
+  return new Response('pong', {
+    headers: { 'content-type': 'text/plain; charset=utf-8' }
+  });
 }
-module.exports = handler;          // CJS
-module.exports.default = handler;  // make sure "default" exists too
-exports.default = handler;
+
